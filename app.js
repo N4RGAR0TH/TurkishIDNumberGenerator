215770df7a53d4f6dbc1463197e76c126a55b72e
N4RGAR0TH <72884047+N4RGAR0TH@users.noreply.github.com>
2022-07-19 01:46:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const success = document.querySelector('.alert-success');
+const notEnoughDigit = document.querySelector('.alert-warning');
+const fail = document.querySelector('.alert-danger');
+
 function generateNumber(){
     numberText = document.getElementById('id-number');
     numID = Math.floor(Math.random() * 999999999) + 100000000;
@@ -16,10 +20,10 @@ function generateNumber(){
     }
     stringID += z % 10;
 
-    if(stringID.length > 11){
+    if(stringID.length > 11) {
         generateNumber();
     }
-    else{
+    else {
         numberText.innerText = stringID;
     }
 }
@@ -27,4 +31,43 @@ function generateNumber(){
 function copyNumber(){
     var numberText = document.getElementById('id-number');
     navigator.clipboard.writeText(numberText.innerText);
+}
+
+function checkNumber(){
+    var stringID = document.getElementById('check-id').value;
+
+    initAlert();
+
+    var x = 0, y = 0, z= 0;
+
+    for (let i = 0; i < 10; i+=2) {
+        x += parseInt(stringID[i]);
+    }
+    for (let i = 1; i < 9; i+=2) {
+        y += parseInt(stringID[i]);
+    }
+
+    tenthDigit = (x * 7 - y) % 10;
+
+    for (let i=0; i<9; i+=1){
+        z += parseInt(stringID[i]);
+    }
+    z += tenthDigit; 
+    eleventhDigit = z % 10;
+
+    if(stringID[9] == tenthDigit && stringID[10] == eleventhDigit){
+        success.classList.remove('d-none');
+    }
+    else if(!(stringID.length == 11)){
+        notEnoughDigit.classList.remove('d-none');
+    }
+    else{
+        fail.classList.remove('d-none');
+    }
+}
+
+function initAlert(){
+    success.classList.add('d-none');
+    notEnoughDigit.classList.add('d-none');
+    fail.classList.add('d-none');
 }
